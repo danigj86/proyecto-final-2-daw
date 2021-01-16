c3fc23f7843bfc97c2415aa0b20cc5328f8dc23f
danigj86 <48217639+danigj86@users.noreply.github.com>
2021-01-16 13:07:05 +0100

    Update mailContact.php

--- mailContact.php
+++ mailContact.php
@@ -20,8 +20,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'guerrerojimenezdaniel@gmail.com';                     // SMTP username
-    $mail->Password   = '-75687568-';                               // SMTP password
+    $mail->Username   = 'PON AQUI TU CORREO GMAIL';                     // SMTP username
+    $mail->Password   = 'PON AQUI TU CONTRASEÑA';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
